4176401d586e59d7e31bd5959f333d1bce7afa0b
Linh Vo <hallinhvo30112008@gmail.com>
2025-07-24 09:57:32 -0400

    idk

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+
+            VStack {
+                Text("🏠 This is the home page")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.red)
+                    .padding(.horizontal, 20.0)
+                NavigationLink(destination: SecondView()) {
+                    Text("Click Me!")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.purple)
+                }
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
         }
         .padding()
     }
---
+++ Navigation/SecondView.swift
@@ -0,0 +1,15 @@
+//
+//  Page A.swift
+//  Navigation
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Yay! You make it to the second view!")
+    }
+}
